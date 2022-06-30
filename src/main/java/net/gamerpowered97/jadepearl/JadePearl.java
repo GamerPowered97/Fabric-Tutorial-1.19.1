de0080108a44352f167b7f0776ff9fb117474a07
Michael <toschimichael22@gmail.com>
2022-06-30 13:55:32 +0200

    commit message

---
+++ src/main/java/net/gamerpowered97/jadepearl/JadePearl.java
@@ -0,0 +1,15 @@
+package net.gamerpowered97.jadepearl;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JadePearl implements ModInitializer {
+	public static final String MOD_ID = "jadepearl";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/gamerpowered97/jadepearl/JadePearlClient.java
@@ -0,0 +1,10 @@
+package net.gamerpowered97.jadepearl;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JadePearlClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
